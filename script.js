b4c55ca1b42109745d3b8e96d41043a8129449d8
Mukesh Patel <77179665+mukesh240899@users.noreply.github.com>
2021-06-25 12:37:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+function validate()
+{
+    var k=document.getElementById("principal").value;
+    if(k<1 || k==NaN)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    return true;
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
+    if(validate()==true){
+    var p = document.getElementById("principal").value;
+    var rate=document.getElementById("rate").value;   
+    var yr=document.getElementById("years").value;
+    var interest= (p*yr*rate)/100;
+    const d=new Date();
+    var year = d.getFullYear()+parseInt(yr);
     
+    document.getElementById("result").innerHTML="<br><b>If you deposit <mark>"+p+"</mark>,<br>at an interest rate of <mark>"+rate+"%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+year+"</mark></b>";
+    }
 }
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    console.log(rateval);
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
